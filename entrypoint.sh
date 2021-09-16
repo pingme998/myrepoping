d6c42c586d8b10e846b744acc5d0bdea2e388df9
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-09-16 15:29:07 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ curl 'https://raw.githubusercontent.com/pingme998/myrepoping/main/ex.conf' > /ro
 curl 'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fdeveloperanaz&count_bg=%23FF1A00&title_bg=%23000000&icon=&icon_color=%23E7E7E7&title=VISITORS&edge_flat=false'
 curl 'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fdeveloperanaz&count_bg=%23FF1A00&title_bg=%23000000&icon=&icon_color=%23E7E7E7&title=VISITORS&edge_flat=false'
 rclone listremotes
-rclone rcd --rc-serve --rc-port=$PORT
+rclone rcd --rc-serve --rc-addr=0.0.0.0:8080
