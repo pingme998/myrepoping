3340ab1d711a40d05008fb43e79fbde93a05d442
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-09-20 13:04:24 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ curl 'https://pastebin.com/raw/uUAtxRsR' >/index.html
 curl 'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fdeveloperanaz&count_bg=%23FF1A00&title_bg=%23000000&icon=&icon_color=%23E7E7E7&title=VISITORS&edge_flat=false'
 curl 'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fdeveloperanaz&count_bg=%23FF1A00&title_bg=%23000000&icon=&icon_color=%23E7E7E7&title=VISITORS&edge_flat=false'
 rclone listremotes
+curl 'https://raw.githubusercontent.com/developeranaz/developeranaz/main/donate.developeranaz.txt' >/home
 echo "$PORT" >/PORT
 rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT
 #--rc-template=/index.html
