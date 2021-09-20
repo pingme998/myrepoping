529f1dff37af66461630ec110b9b655085f177b0
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-09-20 09:03:27 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-curl 'https://raw.githubusercontent.com/pingme998/myrepoping/main/ex.conf' > /root/.config/rclone/rclone.conf
 curl 'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fdeveloperanaz&count_bg=%23FF1A00&title_bg=%23000000&icon=&icon_color=%23E7E7E7&title=VISITORS&edge_flat=false'
 curl 'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fdeveloperanaz&count_bg=%23FF1A00&title_bg=%23000000&icon=&icon_color=%23E7E7E7&title=VISITORS&edge_flat=false'
 rclone listremotes
